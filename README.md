4c36b6948d18a66c19be02f0a5b9b09fa09a7a34
Miguel Jardón <migueljp83@gmail.com>
2017-10-31 16:31:59 +0100

    First commit.

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 An weather application to show the 7 days forecast for any city.
 
---
+----
 
 ###### © Miguel Jardón
--- app/src/main/java/com/ammyt/kguedr/MainActivity.kt
+++ app/src/main/java/com/ammyt/kguedr/MainActivity.kt
@@ -13,5 +13,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Log.v(TAG, "🔎 Testing onCreate.")
+
+        if (savedInstanceState != null) {
+            Log.v(TAG, "savedInstanceState NO es null y vale: ${savedInstanceState.getString("clave")}")
+        }
+        else {
+            Log.v(TAG, "savedInstanceState ES null")
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+
+        Log.v(TAG, "🔎 onSaveInstanceState.")
+
+        outState?.putString("clave", "valor")
     }
 }
