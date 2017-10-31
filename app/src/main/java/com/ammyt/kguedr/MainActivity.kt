ac85aa111cd1c7023395775859e723e0331d24f2
Miguel Jardón <migueljp83@gmail.com>
2017-10-31 16:05:23 +0100

    First commit.

---
+++ app/src/main/java/com/ammyt/kguedr/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.ammyt.kguedr
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    val TAG = MainActivity::class.java.canonicalName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.v(TAG, "🔎 Testing onCreate.")
+    }
+}
