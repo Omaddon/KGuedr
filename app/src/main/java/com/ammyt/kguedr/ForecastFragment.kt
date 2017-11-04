b3f30dde4a590113d43c00b8d703c430e60223cc
Miguel Jardón <migueljp83@gmail.com>
2017-11-04 15:36:28 +0100

    ForecastFRAGMENT before refactoring (NOT compile).

---
+++ app/src/main/java/com/ammyt/kguedr/ForecastFragment.kt
@@ -0,0 +1,21 @@
+package com.ammyt.kguedr
+
+import android.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class ForecastFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater?,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        inflater.let {
+
+        }
+    }
+}
