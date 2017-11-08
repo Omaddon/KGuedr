6ba33af79c04850568f6efd50f7e48bf616ae202
Miguel Jardón <migueljp83@gmail.com>
2017-11-08 15:29:13 +0100

    Multi devices.

--- app/src/main/java/com/ammyt/kguedr/activity/ForecastActivity.kt
+++ app/src/main/java/com/ammyt/kguedr/activity/ForecastActivity.kt
@@ -1,10 +1,12 @@
 package com.ammyt.kguedr.activity
 
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import com.ammyt.kguedr.R
 import com.ammyt.kguedr.fragment.CityListFragment
-import com.ammyt.kguedr.model.Cities
+import com.ammyt.kguedr.fragment.CityPagerFragment
 import com.ammyt.kguedr.model.City
 
 class ForecastActivity : AppCompatActivity(), CityListFragment.OnCitySelectedListener{
@@ -14,14 +16,43 @@ class ForecastActivity : AppCompatActivity(), CityListFragment.OnCitySelectedLis
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forecast)
 
-        // SIEMPRE antes de crear un fragment, debemos comprobar si ya
-        // hemos añadido éste a nuestra jerarquía
-        if (fragmentManager.findFragmentById(R.id.city_list_fragment) == null) {
-            val fragment = CityListFragment.newInstance()
-            fragmentManager.beginTransaction()
-                    .add(R.id.city_list_fragment, fragment)
-                    .commit()
+        // Con esto podemos conocer los detalles físicos del dispositivo que se está ejecutando
+        // Se mostrará aquí mismo en modo depuración los valores
+        // ------------------------------------------------------------
+        val metrics = resources.displayMetrics
+        val width = metrics.widthPixels
+        val height = metrics.heightPixels
+        val dpWidth = (width / metrics.density).toInt()
+        val dpHeight = (height / metrics.density).toInt()
+        val model = Build.MODEL
+        val androidVersion = Build.VERSION.SDK_INT
+        val dpi = metrics.densityDpi
+        // ------------------------------------------------------------
+
+        // Según la orientación y tamaño de nuestro dispositivo, deberemos mostrar unos fragments u otros
+        // Comprobamos qué FrameLayouts (los placeHolder) tenemos
+        if (findViewById<View>(R.id.city_list_fragment) != null) {
+            // SIEMPRE antes de crear un fragment, debemos comprobar si ya
+            // hemos añadido éste a nuestra jerarquía
+            if (fragmentManager.findFragmentById(R.id.city_list_fragment) == null) {
+                val fragment = CityListFragment.newInstance()
+                fragmentManager.beginTransaction()
+                        .add(R.id.city_list_fragment, fragment)
+                        .commit()
+            }
         }
+
+        if (findViewById<View>(R.id.fragment_city_pager) != null) {
+            // SIEMPRE antes de crear un fragment, debemos comprobar si ya
+            // hemos añadido éste a nuestra jerarquía
+            if (fragmentManager.findFragmentById(R.id.fragment_city_pager) == null) {
+                val fragment = CityPagerFragment.newInstance(0)
+                fragmentManager.beginTransaction()
+                        .add(R.id.fragment_city_pager, fragment)
+                        .commit()
+            }
+        }
+
     }
 
     override fun onCitySelected(city: City?, position: Int) {
