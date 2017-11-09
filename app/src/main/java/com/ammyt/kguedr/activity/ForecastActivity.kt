4d1360df4f55d818c4f7eebde0465cb07e1ba0dc
Miguel Jardón <migueljp83@gmail.com>
2017-11-09 17:44:10 +0100

    Fixes (imports). END module Android.

--- app/src/main/java/com/ammyt/kguedr/activity/ForecastActivity.kt
+++ app/src/main/java/com/ammyt/kguedr/activity/ForecastActivity.kt
@@ -6,8 +6,6 @@ import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.widget.Button
-import android.widget.Toast
 import com.ammyt.kguedr.R
 import com.ammyt.kguedr.fragment.CityListFragment
 import com.ammyt.kguedr.fragment.CityPagerFragment
